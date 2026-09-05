b65555bf477395d2803c16f63d07dd10f5d27672
Ivan Guillermo Lopez <ivanglopezp@gmail.com>
2026-09-05 07:47:48 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -58,3 +58,12 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
+// sw.js
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('keepapp-v1').then((cache) => cache.addAll(['./', './index.html'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
