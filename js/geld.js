0367ed29a8a4f9b3b81574c58eea93685b1bfd65
Ivan Guillermo Lopez <ivanglopezp@gmail.com>
2026-09-22 11:27:44 -0400

    Update geld.js

--- js/geld.js
+++ js/geld.js
@@ -3,7 +3,7 @@
  */
 KeepModule('geld', () => {
   const SHEET_ID = '1jw9T6byYopO1uOX3iDTtD_9DFvl_2LaC-tT-Qgsu7kw';
-  const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzOUx8P3Fc1c89B1QiHBQot1f0NahjUSWQ1HuTIwQVbe9ElHZLB2NOEUge8B5-4hhHv/exec';
+  const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyCuvR3oQyqHG5hq8UKkjnPc2MAKDTshQoUKHlUKcfeC43WG0kIOmO6QnlhBIt4aKaD/exec';
 
   const URL_COMPRAS = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&sheet=compras`;
   const URL_GASTOS_CSV = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&sheet=gastos`;
